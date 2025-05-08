f1bbff99f24ee337c5223be87d340ebc4f4ff652
yusongod0303 <yusongod33@gmail.com>
2025-05-09 01:58:49 +0900

    update_experience

--- src/pages/portfolio/components/Experience/index.tsx
+++ src/pages/portfolio/components/Experience/index.tsx
@@ -55,61 +55,6 @@ const Experience = React.forwardRef<HTMLDivElement, ExperienceProps>((props, ref
   };
 
   const contentList: ExperienceContent[] = [
-    /*
-    {
-      datePeriod: 'April 2021 – Current',
-      title: '팀블라인드',
-      subTitle: '서버 개발자',
-      desc: [
-        '팀블라인드에서 운영하는 서비스 서버 개발을 담당했습니다.',
-        'Node.js, Express 기반으로 한 백엔드 개발과',
-        'Vue 프레임워크를 적용한 웹페이지 및 어드민 개발 업무를 수행했습니다.',
-        '이외에도 젠킨스를 활용한 CI/CD 자동화도 경험했습니다.',
-      ].join(' '),
-      projects: [
-        {
-          title: '온라인 취미 클래스 서비스 서버 개발',
-        },
-        {
-          title: '데이팅앱 서비스 서버 개발',
-        },
-      ],
-    },
-    */
-    /*
-    {
-      datePeriod: 'February 2017 – April 2021',
-      title: '유엔젤',
-      subTitle: 'CBS개발부 / 대리',
-      desc: [
-        '통신사 CS, OCS, Provisioning 개발을 담당했습니다.',
-        'Scala, PHP, Node.js 등을 기반으로 하는 다양한 프레임워크, 라이브러리를 활용해 백엔드 개발 업무를 수행했으며',
-        'React를 활용한 클라이언트 개발도 경험했습니다.',
-        '이외에도 ELK, Grafana를 비롯한 오픈소스를 활용해 모니터링 시스템을 구축하고 AWS·OCI 상에서',
-        '서버리스 아키텍처를 구현한 경험이 있습니다.',
-      ].join(' '),
-      projects: [
-        {
-          title: 'Global SIM 서비스 백엔드 개발',
-        },
-        {
-          title: '몽골 MTC IN 선불서비스 개발',
-        },
-        {
-          title: '몽골 MTC 빌링솔루션 CS(Customer Care System) 개발',
-        },
-        {
-          title: 'SKT Smart[Fleet] - Swing 시스템 연동 개발',
-        },
-        {
-          title: 'SKT Smart[Fleet] 플랫폼 개발',
-        },
-        {
-          title: '몽골 Unitel CBS Provisioning SM',
-        },
-      ],
-    },
-    */
     {
       datePeriod: 'May 2025 - Current',
       title: '유비벨록스(주)',
